98228c1b27ae74231e5ce7e8295cf2b9d8a3ce5c
MIT-RareTECH <mitsuki.swimmer@gmail.com>
2026-04-25 22:52:46 +0900

    fix: 完了

--- config/jinja2.py
+++ config/jinja2.py
@@ -1,8 +1,6 @@
 from django.templatetags.static import static
 from django.urls import reverse
-from django.middleware.csrf import get_token
 from jinja2 import Environment
-from markupsafe import Markup
 
 
 def environment(**options):
@@ -11,11 +9,4 @@ def environment(**options):
         'static': static,
         'url': reverse,
     })
-
-    #csrf_inputを{{ csrf_input }}で使えるようにする
-    def csrf_input(request):
-        return Markup(f'<input type="hidden" name="csrfmiddlewaretoken" value="{get_token(request)}">')
-
-    env.globals['csrf_input'] = property(csrf_input)    
     return env
-
